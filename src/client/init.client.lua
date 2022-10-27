ddbdc3d999afa7edb028d38970d362c20c2ee7df
pluviolithic <e-mckanna@onu.edu>
2022-10-27 12:31:26 -0400

    Fix formatting issues with last commit

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -41,75 +41,68 @@ canvas:SetParent(screenGui)
 for x = 1, gridSizeX do
 	grid[x] = {}
 	for y = 1, gridSizeY do
-		grid[x][y] = {customEnums.Air, minMass}
+		grid[x][y] = { customEnums.Air, minMass }
 		canvas:SetPixel(x, y, airColor)
 	end
 end
 
 RunService.Heartbeat:Connect(function()
-
-	local relativeMousePosition = UserInputService:GetMouseLocation() - screenGui.GradientCanvas.AbsolutePosition - Vector2.new(minXPosition, minYPosition)
+	local relativeMousePosition = UserInputService:GetMouseLocation()
+		- screenGui.GradientCanvas.AbsolutePosition
+		- Vector2.new(minXPosition, minYPosition)
 	if not pointIsInFrame(relativeMousePosition) then
 		return
 	end
-    if UserInputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) then
-        local x = math.floor(relativeMousePosition.X / 4)
-        local y = math.floor(relativeMousePosition.Y / 4)
-        grid[x][y] = {customEnums.Ground, maxMass}
-    elseif UserInputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton2) then
-        local x = math.floor(relativeMousePosition.X / 4)
-        local y = math.floor(relativeMousePosition.Y / 4)
-        grid[x][y] = {customEnums.Water, maxMass}
-    end
+	if UserInputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) then
+		local x = math.floor(relativeMousePosition.X / 4)
+		local y = math.floor(relativeMousePosition.Y / 4)
+		grid[x][y] = { customEnums.Ground, maxMass }
+	elseif UserInputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton2) then
+		local x = math.floor(relativeMousePosition.X / 4)
+		local y = math.floor(relativeMousePosition.Y / 4)
+		grid[x][y] = { customEnums.Water, maxMass }
+	end
 
+	-- run simulation
 
-    -- run simulation
+	for x = 1, gridSizeX do
+		for y = gridSizeY, 1, -1 do
+			local cell = grid[x][y]
+			if cell[1] ~= customEnums.Water then
+				continue
+			end
 
-    for x = 1, gridSizeX do
-        for y = gridSizeY, 1, -1 do
-            local cell = grid[x][y]
-            if cell[1] ~= customEnums.Water then
-                continue
-            end
+			local belowCell = grid[x][y + 1]
+			if not belowCell or belowCell[1] ~= customEnums.Ground then
+				if not belowCell then
+					cell[2] = 0
+					cell[1] = customEnums.Air
+				elseif belowCell[2] < maxMass then
+					local transferAmount = math.clamp(maxMass - belowCell[2], minMass, cell[2])
+					cell[2] -= transferAmount
+					belowCell[1] = customEnums.Water
+					belowCell[2] += transferAmount
 
-            local belowCell = grid[x][y + 1]
-            if not belowCell or belowCell[1] ~= customEnums.Ground then
+					if cell[2] == minMass then
+						cell[1] = customEnums.Air
+					end
+				end
+			end
+		end
+	end
 
-                if not belowCell then
-                    cell[2] = 0
-                    cell[1] = customEnums.Air
-                elseif belowCell[2] < maxMass then
+	for x = 1, gridSizeX do
+		for y = 1, gridSizeY do
+			local cell = grid[x][y]
+			if cell[1] == customEnums.Ground then
+				canvas:SetPixel(x, y, groundColor)
+			elseif cell[1] == customEnums.Water then
+				canvas:SetPixel(x, y, Color3.fromHSV(0.6, cell[2] / maxMass, 1))
+			else
+				canvas:SetPixel(x, y, airColor)
+			end
+		end
+	end
 
-                    local transferAmount = math.clamp(maxMass - belowCell[2], minMass, cell[2])
-                    cell[2] -= transferAmount
-                    belowCell[1] = customEnums.Water
-                    belowCell[2] += transferAmount
-
-                    if cell[2] == minMass then
-                        cell[1] = customEnums.Air
-                    end
-
-                end
-
-            end
-
-
-        end
-    end
-
-    for x = 1, gridSizeX do
-        for y = 1, gridSizeY do
-            local cell = grid[x][y]
-            if cell[1] == customEnums.Ground then
-                canvas:SetPixel(x, y, groundColor)
-            elseif cell[1] == customEnums.Water then
-                canvas:SetPixel(x, y, Color3.fromHSV(.6, cell[2] / maxMass, 1))
-            else
-                canvas:SetPixel(x, y, airColor)
-            end
-        end
-    end
-
-    canvas:Render()
-
+	canvas:Render()
 end)
